0501bd4436902984eef97f7a29ffe8b7957dc9c4
Timur Valiev <cptchunk@yandex.ru>
2025-04-15 17:50:59 +0300

    fix: noqa WPS210 in api

--- curlifier/api.py
+++ curlifier/api.py
@@ -6,28 +6,22 @@ from curlifier.curl import Curl
 from curlifier.structures.types import CurlifyRequestConfigure
 
 
-def curlify(  # noqa: WPS210
+def curlify(
     response: Response | None = None,
     *,
     prepared_request: PreparedRequest | None = None,
     build_short: bool,
     **kwargs: Unpack[CurlifyRequestConfigure],
 ) -> str:
-    location = kwargs.pop('location', False)
-    verbose = kwargs.pop('verbose', False)
-    silent = kwargs.pop('silent', False)
-    insecure = kwargs.pop('insecure', False)
-    include = kwargs.pop('include', False)
-
     curl = Curl(
         response=response,
         prepared_request=prepared_request,
         build_short=build_short,
-        location=location,
-        verbose=verbose,
-        silent=silent,
-        insecure=insecure,
-        include=include,
+        location=kwargs.pop('location', False),
+        verbose=kwargs.pop('verbose', False),
+        silent=kwargs.pop('silent', False),
+        insecure=kwargs.pop('insecure', False),
+        include=kwargs.pop('include', False),
     )
 
     return curl.curlify()
