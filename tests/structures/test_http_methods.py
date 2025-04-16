990f2ff9793f7c3d8149d83b62da697be72fc50b
Timur Valiev <cptchunk@yandex.ru>
2025-04-16 13:02:13 +0300

    tests: HttpMethodsEnum

--- tests/structures/test_http_methods.py
+++ tests/structures/test_http_methods.py
@@ -1,2 +1,6 @@
-#  noqa: WPS411
-...
+from curlifier.structures.http_methods import HttpMethodsEnum
+
+
+def test_http_methods_enum():
+    all_methods = HttpMethodsEnum.get_methods_without_body() + HttpMethodsEnum.get_methods_with_body()
+    assert len(all_methods) == len(tuple(method for method in HttpMethodsEnum))
