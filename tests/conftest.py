03e08e7cbdc08e59f49c53fda9618cc919f2b0a5
Timur Valiev <cptchunk@yandex.ru>
2025-04-16 14:08:54 +0300

    tests: ban on any external requests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from typing import NoReturn
+
 import faker
 import pytest
 
@@ -5,3 +7,22 @@ import pytest
 @pytest.fixture(scope='session')
 def fake():
     return faker.Faker()
+
+
+@pytest.fixture(autouse=True)
+def ban_external_requests(monkeypatch) -> NoReturn:
+    """Ban on any external requests."""
+    m_path = 'urllib3.connectionpool.HTTPConnectionPool.urlopen'
+
+    def _ban_external_requests(http_connection_pool, **_):
+        msg = 'Attempting to make a request to `{host}` without a mock'
+        raise RuntimeError(
+            msg.format(
+                host=http_connection_pool.host,
+            )
+        )
+
+    monkeypatch.setattr(
+        m_path,
+        _ban_external_requests,
+    )
