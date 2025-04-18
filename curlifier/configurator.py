4f26a4104a37bc8bc3919312e369db6694cd7b5b
Timur Valiev <cptchunk@yandex.ru>
2025-04-18 17:19:31 +0300

    docs: docstr for `configirator.py`

--- curlifier/configurator.py
+++ curlifier/configurator.py
@@ -5,6 +5,8 @@ from curlifier.structures.types import CurlCommand, EmptyStr
 
 
 class Config:
+    """Parameters for curl command configuration."""
+
     __slots__ = (
         '_location',
         '_verbose',
@@ -29,26 +31,33 @@ class Config:
 
     @property
     def location(self: Self) -> bool:
+        """Follow redirects."""
         return self._location
 
     @property
     def verbose(self: Self) -> bool:
+        """Make the operation more talkative."""
         return self._verbose
 
     @property
     def silent(self: Self) -> bool:
+        """Silent mode."""
         return self._silent
 
     @property
     def insecure(self: Self) -> bool:
+        """Allow insecure server connections."""
         return self._insecure
 
     @property
     def include(self: Self) -> bool:
+        """Include protocol response headers in the output."""
         return self._include
 
 
 class ConfigBuilder(Config):
+    """Builds a curl command configuration line."""
+
     __slots__ = (
         'build_short',
     )
@@ -96,6 +105,23 @@ class ConfigBuilder(Config):
         return ''
 
     def build(self: Self) -> str:
+        """
+        Collects all parameters into the resulting string.
+
+        If `build_short` is `True` will be collected short version.
+
+        >>> from curlifier.configurator import ConfigBuilder
+        >>> conf = ConfigBuilder(
+            location=True,
+            verbose=True,
+            silent=False,
+            insecure=True,
+            include=False,
+            build_short=False,
+        )
+        >>> conf.build()
+        '--location --verbose --insecure'
+        """
         commands: tuple[CurlCommand | EmptyStr, ...] = (
             self.get_location_command(),
             self.get_verbose_command(),
